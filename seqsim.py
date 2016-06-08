7fb215fd4836c488792dba9e107d47bb0a3c3ae0
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-08 19:09:52 +0200

    reads are generated

--- seqsim.py
+++ seqsim.py
@@ -1,41 +1,34 @@
 #! /usr/bin/env python
-
 import random
 
-def BarGen (N):
-	Bases = list ( 'ATGC' )
-	Barcode = [ random.choice ( Bases ) for i in range (N) ]
-	Barcode = ''.join ( Barcode )
-	return Barcode
+Bases = list ( 'ATGC' )
+Barcode = [ random.choice ( Bases ) for i in range (12) ]
+Barcode = ''.join ( Barcode )
 
 #The flow so far is generating a random barcode of 12bp lenght.
 #Now it is required to generate 100 of them and make a list with these 100 unique barcodes.
 
-def BarList(N, M):
-	Barcodes = []
-	for i in range (N):
-		Barcodes.append ( BarGen (M) )
-	print (Barcodes)
-	return BarList
+Barcodes = []
+for i in range (100):
+	Barcodes.append ( Barcode )
 
 #Now we have a list of 100 barcodes.
 #Next aim to write a function that would generate mRNA reads with random lenghts
 
-def mRNAGen (N):
-	Bases = list ( 'ATGC' )
-	mRNA = [ random.choice (Bases) for i in range (N) ]
-	mRNA = ''.join (mRNA)
-	return mRNA
+mRNA = [ random.choice (Bases) for i in range (1000) ]
+mRNA = ''.join (mRNA)
 
-def mRNAlist (N, M):
-	mRNAs = []
-	for i in range (N):
-		mRNAs.append (mRNAGen (M))
-	print (mRNAs)
-	return mRNAs
+#This generates a random mRNA lenght of 1000bp
+#now I have to generate 10000 mRNA reads and each of them should have one of the barcodes that was generated.
 
-#Now I have a library of 100000 random mRNA sequences each is 1000bp long.
-#Next step is to select each one of these mRNA sequences and add a barcode at the beginning of it.
+Read = [ random.choice(Barcodes) + mRNA]
 
-BarList(100,12)
-mRNAlist (100,5)
+#This picks a barcode from the list and concatenates it with a random 1000bp sequence.
+
+DropSeq = []
+for i in range (10000):
+	DropSeq.append (Read)
+
+#This makes a list of 10000 reads which has a barcode and a mRNA.
+
+print(DropSeq)
