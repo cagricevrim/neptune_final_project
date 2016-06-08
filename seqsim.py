eb3e5ed0b53c69cc1111fea0d0db429bf57fb541
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-08 16:38:20 +0200

    list of barcodes has been generated

--- seqsim.py
+++ seqsim.py
@@ -2,9 +2,22 @@
 
 import random
 
-N = 12
+#N = 12
 
-Bases = list('ATGC')
-Barcode = [ random.choice(Bases) for i in range(N) ]
-Barcode = ''.join(Barcode)
-print (Barcode)
+def BarGen (N):
+	Bases = list ( 'ATGC' )
+	Barcode = [ random.choice ( Bases ) for i in range (N) ]
+	Barcode = ''.join ( Barcode )
+	return Barcode
+
+#The flow so far is generating a random barcode of 12bp lenght.
+#Now it is required to generate 100 of them and make a list with these 100 unique barcodes.
+
+Barcodes = [ ]
+
+for i in range (100):
+	Barcodes.append ( BarGen (12) )
+	
+print (Barcodes)
+
+#Now we have a list of 100 barcodes.
