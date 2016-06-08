14481d19b272efeafc0faee367a94d0fe999fd0e
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-08 17:21:35 +0200

    random mRNA generator

--- seqsim.py
+++ seqsim.py
@@ -2,8 +2,6 @@
 
 import random
 
-#N = 12
-
 def BarGen (N):
 	Bases = list ( 'ATGC' )
 	Barcode = [ random.choice ( Bases ) for i in range (N) ]
@@ -13,11 +11,31 @@ def BarGen (N):
 #The flow so far is generating a random barcode of 12bp lenght.
 #Now it is required to generate 100 of them and make a list with these 100 unique barcodes.
 
-Barcodes = [ ]
-
-for i in range (100):
-	Barcodes.append ( BarGen (12) )
-	
-print (Barcodes)
+def BarList(N, M):
+	Barcodes = []
+	for i in range (N):
+		Barcodes.append ( BarGen (M) )
+	print (Barcodes)
+	return BarList
 
 #Now we have a list of 100 barcodes.
+#Next aim to write a function that would generate mRNA reads with random lenghts
+
+def mRNAGen (N):
+	Bases = list ( 'ATGC' )
+	mRNA = [ random.choice (Bases) for i in range (N) ]
+	mRNA = ''.join (mRNA)
+	return mRNA
+
+def mRNAlist (N, M):
+	mRNAs = []
+	for i in range (N):
+		mRNAs.append (mRNAGen (M))
+	print (mRNAs)
+	return mRNAs
+
+#Now I have a library of 100000 random mRNA sequences each is 1000bp long.
+#Next step is to select each one of these mRNA sequences and add a barcode at the beginning of it.
+
+BarList(100,12)
+mRNAlist (100,5)
