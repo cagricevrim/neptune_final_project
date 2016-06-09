da89a5fd8e6c11696e4ad91a9698291b88d5d1c6
Cagri Cevrim <cagri.cevrim@ens-lyon.fr>
2016-06-09 17:39:46 +0200

    Two functions are combined in seqsim.py

--- seqsim.py
+++ seqsim.py
@@ -2,30 +2,49 @@
 import random
 
 
-def seqsim (X, Y, Z, T):
+def seqsim (A, B, D, C):
 	file = open ( 'SeqSimulation.txt', 'w' )
 	Bases = list ( 'ATGC' )
 	Barcodes = []
 	DropSeq = []
 
-	for i in range (X):
-		Barcode = [ random.choice ( Bases ) for i in range (Y) ]
+	for i in range (A):
+		Barcode = [ random.choice ( Bases ) for i in range (B) ]
 		Barcode = ''.join ( Barcode )
 		Barcodes.append ( Barcode )
 
-	for i in range (Z):
-		mRNA = [ random.choice (Bases) for i in range (T) ]
+	for i in range (D):
+		mRNA = [ random.choice (Bases) for i in range (C) ]
 		mRNA = ''.join (mRNA)
 		Read = ( random.choice(Barcodes) + mRNA + '\n')
 		file.write ( Read )
 
 	file.close()
 
-Y = int(raw_input ('Barcode size '))
-X = int(raw_input ('Number of barcodes '))
-T = int(raw_input ('Lenght of mRNA '))
-Z = int(raw_input ('Number of mRNAs reads '))
+B = int(raw_input ('Barcode size '))
+A = int(raw_input ('Number of barcodes '))
+C = int(raw_input ('Lenght of mRNA '))
+D = int(raw_input ('Number of mRNAs reads '))
 
-seqsim (X, Y, Z, T)
+seqsim (A, B, D, C)
 
 print ('Simulations are saved in SeqSimulation.txt in your directory')
+
+H = raw_input ( 'Would you like the sequences to be sorted according to barcodes Y/N ? ' )
+
+def seqsorter ( X ):
+	f = open ( X, 'r' )
+	file = open ( 'SortedSeq.txt', 'w' )
+
+	lines = f.readlines ()
+	Y = sorted ( lines )
+	for item in Y:
+		file.write ( item )
+
+	f.close()
+	file.close()
+
+	print ( 'The Sequences are sorted and saved in SortedSeq.txt in your directory')
+
+if H == ( 'Y' ):
+	seqsorter ( 'SeqSimulation.txt')
